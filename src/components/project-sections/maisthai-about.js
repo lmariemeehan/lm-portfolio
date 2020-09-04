25dc87d52b7d4c1ffc355ac464af1883091fb5f7
Lola <lmariemeehan@gmail.com>
2020-09-03 19:41:11 -0700

    Revised mais thai about section

--- src/components/project-sections/maisthai-about.js
+++ src/components/project-sections/maisthai-about.js
@@ -6,8 +6,7 @@ function MaisThaiAbout() {
         <>
             <hr />
             <p className="m-3"><span className="mr-2 font-weight-bold">About: </span>
-                I love this restaurant. Little hole in the wall Thai place that I have been going to for a little over a decade. It is owned by a very sweet family that is not very tech savvy 
-                which is why they have never had a website. So I built them this.
+                I love this restaurant. Little hole in the wall Thai place that I have been going to for a little over a decade. It is owned and operated by a very sweet family that has been in business for over 30 years. With the beginning of the awareness of Covid in my area, I thought that businesses such as restaurants that did not currently have a website could benefit from having one. So I looked around and noticed that my favorite Thai place did not have one. At the time, I dabbled with Wordpress and wanted more practice so with permission I built them this.
             </p>
             <p className="m-3"><span className="mr-2 font-weight-bold">Built with: </span> 
                 Wordpress and CSS.
